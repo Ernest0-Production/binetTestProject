4a17631ebdaf22056c3c4845081b120353016970
Ernest0 <magatar136@yandex.ru>
2019-05-05 17:36:24 +0300

    Initial Commit

---
+++ binetTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  binetTestProject
+//
+//  Created by Бабаян Эрнест on 05/05/2019.
+//  Copyright © 2019 Бабаян Эрнест. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
